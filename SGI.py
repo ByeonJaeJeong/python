38a7f7438c326d6afe0a90445f19c5e39a982708
Jeong <qsz78547@naver.com>
2021-02-03 14:12:41 +0900

    path_action crawling on off 추가할내용 저장하기

--- SGI.py
+++ SGI.py
@@ -1,4 +1,5 @@
 from tkinter import filedialog
+from tkinter import messagebox
 import tkinter.ttk
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,6 +10,9 @@ import tkinter as tk
 import threading,queue
 
 class MainFrame(tk.Tk):
+    driver = ''
+    images = ''
+    q = ''
     def __init__(self):
         tk.Tk.__init__(self)
         self.geometry("500x300+100+100")
@@ -19,9 +23,9 @@ class MainFrame(tk.Tk):
         saveFrame.pack(side="top", fill="both", expand=False, padx=5, pady=2,)
 
         #위치 적히는 entry
-        pathEntry = tk.Entry(saveFrame, bd=2, width=60)
-        pathEntry.pack(pady=5)
-
+        self.pathEntry = tk.Entry(saveFrame, bd=2, width=60)
+        self.pathEntry.pack(pady=5)
+        self.pathEntry.bind("<Button-1>", self.path_click)
         #검색하는 프레임
         searchFrame=tk.LabelFrame(self, text="검색 옵션", relief="solid", width="500", height="200", bd=1)
         searchFrame.pack(side="top", fill="both", expand=False, padx=5, pady=2)
@@ -30,40 +34,44 @@ class MainFrame(tk.Tk):
         attribute_Frame.pack(side="top",fill="both", expand=True, padx=5, pady=10)
         #검색 속성
         #Img Size
-        size_combobox=tk.ttk.Combobox(attribute_Frame, width=10, state="readonly", values=[
-            "모든크기", "큼", "중간", "아이콘"
+        self.size_combobox=tk.ttk.Combobox(attribute_Frame, width=10, state="readonly", values=[
+            "모든 크기", "큼", "중간", "아이콘"
         ])
-        size_combobox.grid(column=0, row=0, padx=10, pady=5)
-        size_combobox.current(0)
+        self.size_combobox.grid(column=0, row=0, padx=10, pady=5)
+        self.size_combobox.current(0)
         #색상 빨주노초파남보분흰흑검갈
-        color_combobox=tk.ttk.Combobox(attribute_Frame, width=10, state="readonly", values=[
-            "모든색상", "흑백", "투명", "빨강", "주황", "노랑", "초록",
-            "파랑", "남색", "보라색", "분홍색", "흰색", "회색", "검정색", "갈색"
+        self.color_combobox=tk.ttk.Combobox(attribute_Frame, width=10, state="readonly", values=[
+            "모든 색상", "흑백", "투명", "빨강색", "주황색", "노랑색", "초록색",
+            "청록색", "파랑색", "보라색", "분홍색", "흰색", "회색", "검정색", "갈색"
         ])
 
-        color_combobox.grid(column=1, row=0, padx=10, pady=5)
-        color_combobox.current(0)
+        self.color_combobox.grid(column=1, row=0, padx=10, pady=5)
+        self.color_combobox.current(0)
         #유형 클립아트 선화 gif
-        type_combobox=tk.ttk.Combobox(attribute_Frame, width=10, state="readonly",values=[
-            "모든유형", "클립아트", "선화", "GIF"
+        self.type_combobox=tk.ttk.Combobox(attribute_Frame, width=10, state="readonly",values=[
+            "모든 유형", "클립아트", "선화", "GIF"
         ])
-        type_combobox.grid(column=2, row=0, padx=10, pady=5)
-        type_combobox.current(0)
+        self.type_combobox.grid(column=2, row=0, padx=10, pady=5)
+        self.type_combobox.current(0)
         #기간 1일 , 1주 1달 1년
-        time_combobox=tk.ttk.Combobox(attribute_Frame, width=10, state="readonly", values=[
-            "모든시간", "1시간", "1일", "1달", "1년"
+        self.time_combobox=tk.ttk.Combobox(attribute_Frame, width=10, state="readonly", values=[
+            "모든 시간", "1일", "1주", "1달", "1년"
         ])
-        time_combobox.grid(column=3, row=0, padx=10, pady=5)
-        time_combobox.current(0)
+        self.time_combobox.grid(column=3, row=0, padx=10, pady=5)
+        self.time_combobox.current(0)
 
         search_btnFrame=tk.Frame(searchFrame, width=500, height=50, relief="solid")
         search_btnFrame.pack(side="top", fill="both", expand=False, padx=5, pady=10)
+        #검색창
+        self.search_Entry=tk.Entry(search_btnFrame, width=45, bd=2)
+        self.search_Entry.pack(side="left",padx=3)
+
+        #정지버튼
+        self.search_stopButton = tk.Button(search_btnFrame, text="정지", width="8", state=tk.DISABLED, command=self.search_stop_event)
+        self.search_stopButton.pack(side="right", padx=3)
         #검색버튼
-        search_stopButton = tk.Button(search_btnFrame, text="정지", width="8", state=tk.DISABLED)
-        search_stopButton.pack(side="right", padx=3)
-        #중지버튼
-        search_startButton = tk.Button(search_btnFrame, text="검색", width="8")
-        search_startButton.pack(side="right", padx=3)
+        self.search_startButton = tk.Button(search_btnFrame, text="검색", width="8", command=self.search_event)
+        self.search_startButton.pack(side="right", padx=3)
 
 
 
@@ -74,23 +82,118 @@ class MainFrame(tk.Tk):
         progressbar = tk.ttk.Progressbar(stateFrame, maximum=2000, length=480)
         progressbar.grid(column=0, row=1, columnspan=20, pady=5, padx=5)
         #현재진행정도
-        progressLabel=tk.Label(stateFrame,text="0")
-        progressLabel.grid(column=0,row=0)
+        self.progressLabel=tk.Label(stateFrame,text="0")
+        self.progressLabel.grid(column=0,row=0)
         #언더바
         underbarLabel=tk.Label(stateFrame,text="/")
         underbarLabel.grid(column=1,row=0)
         #전체 사진수
-        progressMaxLabel=tk.Label(stateFrame,text="0")
-        progressMaxLabel.grid(column=2,row=0)
+        self.progressMaxLabel=tk.Label(stateFrame,text="0")
+        self.progressMaxLabel.grid(column=2,row=0)
         #시작 정지 버튼 프레임
         actionFrame=tk.Frame(self, relief="solid", width="500", height="50")
         actionFrame.pack(side="top", fill="both", expand=False, padx=4, pady=10)
         # 중지 버튼
-        stopButton = tk.Button(actionFrame, text="정지", width="8", state=tk.DISABLED)
-        stopButton.pack(side="right", padx=3)
+        self.stopButton = tk.Button(actionFrame, text="정지", width="8", state=tk.DISABLED)
+        self.stopButton.pack(side="right", padx=3)
         # 시작 버튼
-        startButton = tk.Button(actionFrame, text="다운로드", width="8")
-        startButton.pack(side="right", padx=3, anchor="s")
+        self.startButton = tk.Button(actionFrame, text="다운로드", width="8")
+        self.startButton.pack(side="right", padx=3, anchor="s")
+    #저장위치 설정 클릭시 event 처리
+    def path_click(self,event):
+        filePath = filedialog.askdirectory(initialdir="/", title='Please select a directory')
+        self.pathEntry.delete(0, "end")
+        self.pathEntry.insert(0, filePath)
+    #검색 속성 가져오기
+    def search_stop_event(self):
+        global driver
+        driver.quit()
+        self.search_startButton.configure(state=tk.NORMAL)
+        self.search_stopButton.configure(state=tk.DISABLED)
+
+    def search_event(self):
+        #옵션값
+        size = self.size_combobox.get()
+        color = self.color_combobox.get()
+        type = self.type_combobox.get()
+        _time = self.time_combobox.get()
+        #검색할 내용
+        search_name = self.search_Entry.get()
+        #결과를 확인하기 위한 Queue
+        q = queue.Queue()
+
+        crawling_t = threading.Thread(target=MainFrame.crawling, args=(self,search_name, size, color, type, _time, q), daemon=True)
+        crawling_t.start()
+        self.search_startButton.configure(state=tk.DISABLED)
+        self.search_stopButton.configure(state=tk.NORMAL)
+    def crawling(self,name, size, color, type, _time, q):
+        #옵션값 dict
+        size_dict={"모든 크기": "", "큼": "isz:l", "중간": "isz:m", "아이콘": "isz:i"}
+        type_dict={"모든 유형": "" , "클립아트": "itp:clipart%2C", "선화": "itp:lineart%2C", "GIF": "itp:animated%2C"}
+        time_dict={"모든 시간": "", "1일": "qdr:d%2C", "1주": "qdr:w%2C", "1달": "qdr:m%2C", "1년": "qdr=y%2C"}
+        color_dict={"모든 색상": "", "흑백": "ic:gray%2C" , "투명": "ic:trans%2C", "빨강색": "ic:specific%2Cisc:red%2C", "주황색": "ic:specific%2Cisc:orange%2C",
+                    "노랑색": "ic:specific%2Cisc:yellow%2C", "초록색": "ic:specific%2Cisc:green%2C", "파랑색": "ic:specific%2Cisc:blue%2C",
+                    "청록색": "ic:specific%2Cisc:teal%2C", "보라색": "ic:specific%2Cisc:purple%2C", "분홍색": "ic:specific%2Cisc:pink%2C",
+                    "흰색": "white%2C", "회색": "ic:specific%2Cisc:gray%2C", "검정색": "ic:specific%2Cisc:black%2C", "갈색": "ic:specific%2Cisc:brown%2C"}
+        #스크롤 이벤트
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        options.add_argument('window-size=1920x1080')
+        options.add_argument("disable-gpu")
+        global driver
+        driver = webdriver.Chrome()
+        pageUrl="http://www.google.co.kr/search?q="+name+"&tbm=isch&hl=ko&tbs="+color_dict[color]+time_dict[_time]+type_dict[type]+size_dict[size]
+        result = pageUrl[len(pageUrl)-3:len(pageUrl)]
+        if result == "%2C":
+            pageUrl=pageUrl[:-3]
+        driver.get(pageUrl)
+
+        # 검색명
+
+
+
+
+        # 스크롤
+        def scroll(driver):
+            SCROLL_PAUSE_TIME = 1
+
+            # Get scroll height
+            last_height = driver.execute_script("return document.body.scrollHeight")
+
+            while True:
+                # Scroll down to bottom
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                # Wait to load page
+                time.sleep(SCROLL_PAUSE_TIME)
+                # driver.implicitly_wait(20)
+                # Calculate new scroll height and compare with last scroll height
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                # 내린후 스크롤과 현재스크롤 비교
+                if new_height == last_height:
+                    try:
+                        # 더보기 클릭 오류나면 종료
+                        driver.find_element_by_css_selector('.mye4qd').click()
+                        continue
+                    except:
+                        print("스크롤 끝")
+                    break
+                last_height = new_height
+        scroll(driver)
+
+        # 이미지 위치정보
+        global images
+        images = driver.find_elements_by_css_selector('.isv-r.PNCib.MSM1fd.BUooTd')
+        print("스레드종료 반환값 : "+str(len(images)))
+        self.progressMaxLabel.configure(text=str(len(images)))
+        self.search_startButton.configure(state=tk.NORMAL)
+        self.search_stopButton.configure(state=tk.DISABLED)
+        messagebox.showinfo("검색완료","총파일수 : "+str(len(images)))
+
+
+
+
+
+
 
 
 
